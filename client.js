3a36e4b3b32869d99bb51c0fd2dff539951b3048
Kenan Scott <kenan@kenanscott.com>
2017-08-01 12:43:01 -0600

    Change to POST request

--- client.js
+++ client.js
@@ -21,13 +21,15 @@ var circle = L.circle([39.376089, -104.853487], {
 
 function updateMap() {
 $.ajax({
+  type: 'POST',
   url: '/rest/live',
   data: {
     lastUpdated: null
   },
   success: function( result ) {
     console.log(result);
-  }
+  },
+  dataType: 'json'
 });
 }
 
