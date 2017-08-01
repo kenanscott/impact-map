089461c2c6fc30738902bd15b344d8eb93762e31
Kenan Scott <kenan@kenanscott.com>
2017-08-01 12:15:45 -0600

    Fix indents

--- client.js
+++ client.js
@@ -6,10 +6,10 @@ mymap.setView(new L.LatLng(38.1711269, -97.5383369), 5);
 
 // Sets the tile layer from Mapbox
 L.tileLayer('https://api.mapbox.com/styles/v1/brandonyates/cj5tvlwng020z2qr7ws0ylg1o/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYnJhbmRvbnlhdGVzIiwiYSI6ImNpcTl3a3AzbDAxbmhmeW0xaGYwbmIwNmQifQ.ItJcFDmazEMy-2spCUZrrA', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 10,
-    id: 'mapbox.streets'
-  }).addTo(mymap);
+  attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+  maxZoom: 10,
+  id: 'mapbox.streets'
+}).addTo(mymap);
 
 
   var circle = L.circle([39.376089, -104.853487], {
