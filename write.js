300a38b3ecda8ac176a8774fd43bbf0156c67d1c
kenanscott <kenan@kenanscott.com>
2017-08-01 22:16:24 -0600

    Split IP string and log

--- write.js
+++ write.js
@@ -4,4 +4,5 @@ var AWS = require("aws-sdk");
 exports.handler = (event, context, callback) => {
     console.log(event);
     callback(null, 'Hello from Lambda');
+    console.log(event.sourceIP.split(',', 1)[0])
 };
