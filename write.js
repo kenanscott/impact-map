af762f6ceaddfe66beb5a63bb0a8a7804e670d37
kenanscott <kenan@kenanscott.com>
2017-08-01 22:25:09 -0600

    Change position of sourceIP log

--- write.js
+++ write.js
@@ -3,6 +3,6 @@ var AWS = require("aws-sdk");
 
 exports.handler = (event, context, callback) => {
     console.log(event);
-    callback(null, 'Hello from Lambda');
     console.log(event.sourceIP.split(',', 1)[0])
+    callback(null, 'Hello from Lambda');
 };
