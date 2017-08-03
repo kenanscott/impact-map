0153933ecae34c8037fdc38594385ff11ca85798
Kenan Scott <kenan@kenanscott.com>
2017-08-02 21:50:35 -0600

    Add table scanning

--- read.js
+++ read.js
@@ -1,8 +1,57 @@
 /*jshint esversion: 6 */
-'use strict'
+'use strict';
 const AWS = require("aws-sdk");
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = (event, context, callback) => {
-  
+    const today = new Date();
+    var yyyy = today.getFullYear();
+    var mm = today.getMonth();
+    var dd = today.getDate();
+    const todayEpoch = new Date(yyyy, mm, dd).getTime() / 1000;
+    console.log(todayEpoch);
+    // 21600
+    
+    let startTime = todayEpoch - 21600;
+    
+    if (event.lastupdated) {
+        startTime = event.lastupdated;
+    }
+    
+    var params = {
+    TableName: "impact-map",
+    ProjectionExpression: "Coordinates, #b",
+    FilterExpression: "#a > :start",
+    ExpressionAttributeNames: {
+        "#a": "Added",
+        "#b": "Action"
+    },
+    ExpressionAttributeValues: {
+         ":start": todayEpoch
+    }
+    };
+
+    console.log("Scanning table.");
+    dynamodb.scan(params, onScan);
+    
+    function onScan(err, data) {
+        if (err) {
+            console.error("Unable to scan the table. Error JSON:", JSON.stringify(err, null, 2));
+        } else {
+            // print all items
+            console.log("Scan succeeded.");
+            data.Items.forEach(function(item) {
+               console.log(item.Coordinates);
+               console.log(item.Action);
+            });
+    
+            // continue scanning if we have more items, because
+            // scan can retrieve a maximum of 1MB of data
+            if (typeof data.LastEvaluatedKey != "undefined") {
+                console.log("Scanning for more...");
+                params.ExclusiveStartKey = data.LastEvaluatedKey;
+                dynamodb.scan(params, onScan);
+            }
+        }
+    }
 };
