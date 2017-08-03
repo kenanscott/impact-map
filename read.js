32c4ca2aaa12886a2aed20c5ec95c07cbf5b0148
kenanscott <kenan@kenanscott.com>
2017-08-02 19:26:05 -0600

    Create read function

---
+++ read.js
@@ -0,0 +1,8 @@
+/*jshint esversion: 6 */
+'use strict'
+const AWS = require("aws-sdk");
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = (event, context, callback) => {
+  
+};
