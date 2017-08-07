8691366cd5d6a68374d6acf8784a4f92c2f6c7fe
Kenan Scott <kenan@kenanscott.com>
2017-08-07 11:08:23 -0600

    Remove data type declaration on startTimeString

--- read.js
+++ read.js
@@ -5,62 +5,60 @@ const AWS = AWSXRay.captureAWS(require('aws-sdk'));
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = (event, context, callback) => {
-    const today = new Date();
-    var yyyy = today.getFullYear();
-    var mm = today.getMonth();
-    var dd = today.getDate();
-    const todayEpoch = new Date(yyyy, mm, dd).getTime() / 1000;
-    console.log(todayEpoch);
+  const today = new Date();
+  var yyyy = today.getFullYear();
+  var mm = today.getMonth();
+  var dd = today.getDate();
+  const todayEpoch = new Date(yyyy, mm, dd).getTime() / 1000;
+  console.log(todayEpoch);
 
-    let startTime = todayEpoch + 21600;
-    console.log('startTime = ' + startTime);
-    if (event.lastupdated > 1) {
-        console.log('Detecting lastupdated variable, ' + event.lastupdated);
-        startTime = Number(event.lastupdated);
-    }
-    console.log('startTime = ' + startTime);
-    let startTimeString = startTime.toString([10]);
+  let startTime = todayEpoch + 21600;
+  console.log('startTime = ' + startTime);
+  if (event.lastupdated > 1) {
+    console.log('Detecting lastupdated variable, ' + event.lastupdated);
+    startTime = Number(event.lastupdated);
+  }
+  console.log('startTime = ' + startTime);
+  let startTimeString = startTime.toString([10]);
 
-    console.log('startTimeString = ' + startTimeString);
+  console.log('startTimeString = ' + startTimeString);
 
-    var params = {
+  var params = {
     TableName: "impact-map",
     ProjectionExpression: "Coordinates, #b",
     FilterExpression: "#a > :start",
     ExpressionAttributeNames: {
-        "#a": "Added",
-        "#b": "Action"
+      "#a": "Added",
+      "#b": "Action"
     },
     ExpressionAttributeValues: {
-         ":start": {
-           "N": startTimeString
-         }
+      ":start": startTimeString
     }
-    };
+  };
 
-let combinedData = [];
+  let combinedData = [];
 
-    console.log("Scanning table.");
-    dynamodb.scan(params, onScan);
+  console.log("Scanning table.");
+  dynamodb.scan(params, onScan);
 
-    function onScan(err, data) {
-        if (err) {
-            console.error("Unable to scan the table. Error JSON:", JSON.stringify(err, null, 2));
-        } else {
-            // print all items
-            console.log("Scan succeeded.");
-            data.Items.forEach(function(item) {
-               combinedData = combinedData.concat(item);
-            });
+  function onScan(err, data) {
+    if (err) {
+      console.error("Unable to scan the table. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+      // print all items
+      console.log("Scan succeeded.");
+      data.Items.forEach(function(item) {
+        combinedData = combinedData.concat(item);
+      });
 
-            // continue scanning if we have more items, because
-            // scan can retrieve a maximum of 1MB of data
-            if (typeof data.LastEvaluatedKey != "undefined") {
-                console.log("Scanning for more...");
-                params.ExclusiveStartKey = data.LastEvaluatedKey;
-                dynamodb.scan(params, onScan);
-            }
-            callback(null, combinedData);
-        }
+      // continue scanning if we have more items, because
+      // scan can retrieve a maximum of 1MB of data
+      if (typeof data.LastEvaluatedKey != "undefined") {
+        console.log("Scanning for more...");
+        params.ExclusiveStartKey = data.LastEvaluatedKey;
+        dynamodb.scan(params, onScan);
+      }
+      callback(null, combinedData);
     }
+  }
 };
