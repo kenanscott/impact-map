663172bc97ec64aa8ad9b435a0703c3a5e9f4819
Kenan Scott <kenan@kenanscott.com>
2017-09-25 12:56:31 -0600

    Remove Google Maps JS and add Google Charts - Geocharts

--- client.js
+++ client.js
@@ -1,245 +1,40 @@
 var map = null;
 var legend = document.getElementById('legend');
+var lastUpdated;
 
 function initMap() {
-  // Create a map object and specify the DOM element for display.
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {
-      lat: 38.922239,
-      lng: -95.794675
-    },
-    disableDefaultUI: true,
-    styles: styles,
-    zoom: 5
-  });
-  map.controls[google.maps.ControlPosition.RIGHT_TOP].push(legend);
 
+  google.charts.load('current', {
+  'packages': ['geochart'],
+  'mapsApiKey': 'AIzaSyDZPziPjsTK8hiOlr1W-uQiDjIRe87FFP4'
+});
 }
-
-var styles = [{
-    "elementType": "geometry",
-    "stylers": [{
-      "color": "#f5f5f5"
-    }]
-  },
-  {
-    "elementType": "labels.icon",
-    "stylers": [{
-      "visibility": "off"
-    }]
-  },
-  {
-    "elementType": "labels.text.fill",
-    "stylers": [{
-      "color": "#616161"
-    }]
-  },
-  {
-    "elementType": "labels.text.stroke",
-    "stylers": [{
-      "color": "#f5f5f5"
-    }]
-  },
-  {
-    "featureType": "administrative.land_parcel",
-    "stylers": [{
-      "visibility": "off"
-    }]
-  },
-  {
-    "featureType": "administrative.land_parcel",
-    "elementType": "labels.text.fill",
-    "stylers": [{
-      "color": "#bdbdbd"
-    }]
-  },
-  {
-    "featureType": "administrative.neighborhood",
-    "stylers": [{
-      "visibility": "off"
-    }]
-  },
-  {
-    "featureType": "poi",
-    "elementType": "geometry",
-    "stylers": [{
-      "color": "#eeeeee"
-    }]
-  },
-  {
-    "featureType": "poi",
-    "elementType": "labels.text",
-    "stylers": [{
-      "visibility": "off"
-    }]
-  },
-  {
-    "featureType": "poi",
-    "elementType": "labels.text.fill",
-    "stylers": [{
-      "color": "#757575"
-    }]
-  },
-  {
-    "featureType": "poi.park",
-    "elementType": "geometry",
-    "stylers": [{
-      "color": "#e5e5e5"
-    }]
-  },
-  {
-    "featureType": "poi.park",
-    "elementType": "labels.text.fill",
-    "stylers": [{
-      "color": "#9e9e9e"
-    }]
-  },
-  {
-    "featureType": "road",
-    "stylers": [{
-      "visibility": "off"
-    }]
-  },
-  {
-    "featureType": "road",
-    "elementType": "geometry",
-    "stylers": [{
-      "color": "#ffffff"
-    }]
-  },
-  {
-    "featureType": "road",
-    "elementType": "labels",
-    "stylers": [{
-      "visibility": "off"
-    }]
-  },
-  {
-    "featureType": "road.arterial",
-    "elementType": "labels.text.fill",
-    "stylers": [{
-      "color": "#757575"
-    }]
-  },
-  {
-    "featureType": "road.highway",
-    "elementType": "geometry",
-    "stylers": [{
-      "color": "#dadada"
-    }]
-  },
-  {
-    "featureType": "road.highway",
-    "elementType": "labels.text.fill",
-    "stylers": [{
-      "color": "#616161"
-    }]
-  },
-  {
-    "featureType": "road.local",
-    "elementType": "labels.text.fill",
-    "stylers": [{
-      "color": "#9e9e9e"
-    }]
-  },
-  {
-    "featureType": "transit.line",
-    "elementType": "geometry",
-    "stylers": [{
-      "color": "#e5e5e5"
-    }]
-  },
-  {
-    "featureType": "transit.station",
-    "elementType": "geometry",
-    "stylers": [{
-      "color": "#eeeeee"
-    }]
-  },
-  {
-    "featureType": "water",
-    "elementType": "geometry",
-    "stylers": [{
-      "color": "#c9c9c9"
-    }]
-  },
-  {
-    "featureType": "water",
-    "elementType": "labels.text",
-    "stylers": [{
-      "visibility": "off"
-    }]
-  },
-  {
-    "featureType": "water",
-    "elementType": "labels.text.fill",
-    "stylers": [{
-      "color": "#9e9e9e"
-    }]
-  }
-];
-
-
-var pointStyles = {
-  'view': {
-    'color': '#000000',
-    'fillColor': '#000000',
-    'fillOpacity': 0.5,
-    'radius': 35000,
-    'pane': 'overlayPane'
-  },
-  'commitment': {
-    'color': 'red',
-    'fillColor': '#f03',
-    'fillOpacity': 0.5,
-    'radius': 75000,
-    'pane': 'markerPane'
-  }
-};
-
-var pageviews = 0;
-var commitments = 0;
-
-var lastUpdated = null;
 
 // Displays the points data provided.
 function displayPoints(data) {
 
   return new Promise(function(resolve, reject) {
 
-    for (var i = 0; i < data.length; i++) {
+ var data = google.visualization.arrayToDataTable([
+   ['Country',   'Population', 'Area Percentage'],
+   ['France',  65700000, 50],
+   ['Germany', 81890000, 27],
+   ['Poland',  38540000, 23]
+ ]);
 
-      // Create LatLng object
-      var LatLng = new google.maps.LatLng({
-        lat: data[i].Coordinates[0],
-        lng: data[i].Coordinates[1]
-      });
+ var options = {
+   sizeAxis: { minValue: 0, maxValue: 100 },
+   region: '155', // Western Europe
+   displayMode: 'markers',
+   colorAxis: {colors: ['#e7711c', '#4374e0']} // orange to blue
+ };
 
-      var circle = new google.maps.Circle({
-        fillColor: pointStyles[data[i].Action].fillColor,
-        fillOpacity: pointStyles[data[i].Action].fillOpacity,
-        map: map,
-        center: LatLng,
-        radius: pointStyles[data[i].Action].radius,
-        strokeWeight: 0
-      });
-
-      if (data[i].Action === 'view') {
-        pageviews++;
-      }
-      if (data[i].Action === 'commitment') {
-        commitments++;
-      }
-    }
-    document.getElementById("pageviews").innerText = pageviews;
-    document.getElementById("commitments").innerText = commitments;
-    resolve('All points processed');
-  });
-
+ var chart = new google.visualization.GeoChart(document.getElementById('chart_div'));
+ chart.draw(data, options);
+ resolve('All points processed');
+});
 }
 
-
-// Updates map with latest real time data
 // https://developers.google.com/web/fundamentals/getting-started/primers/promises
 function get(url) {
 
