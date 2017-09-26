2d41eeb96462c17f9543dab91756d682588648d0
Kenan Scott <kenan@kenanscott.com>
2017-09-26 09:27:55 -0600

    Revert to previous version

--- client.js
+++ client.js
@@ -1,49 +1,245 @@
+var map = null;
 var legend = document.getElementById('legend');
-var lastUpdated;
-var chart;
-var table;
 
-function init() {
-  chart = new google.visualization.GeoChart(document.getElementById('chart_div'));
-  table = new google.visualization.DataTable();
-  table.addColumn('number', 'Lat');
-  table.addColumn('number', 'Lng');
-  table.addColumn('number', 'Commitments'); // Color
-  table.addColumn('number', 'Pageviews'); // Size
-  refreshData();
+function initMap() {
+  // Create a map object and specify the DOM element for display.
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {
+      lat: 38.922239,
+      lng: -95.794675
+    },
+    disableDefaultUI: true,
+    styles: styles,
+    zoom: 5
+  });
+  map.controls[google.maps.ControlPosition.RIGHT_TOP].push(legend);
+
 }
+
+var styles = [{
+    "elementType": "geometry",
+    "stylers": [{
+      "color": "#f5f5f5"
+    }]
+  },
+  {
+    "elementType": "labels.icon",
+    "stylers": [{
+      "visibility": "off"
+    }]
+  },
+  {
+    "elementType": "labels.text.fill",
+    "stylers": [{
+      "color": "#616161"
+    }]
+  },
+  {
+    "elementType": "labels.text.stroke",
+    "stylers": [{
+      "color": "#f5f5f5"
+    }]
+  },
+  {
+    "featureType": "administrative.land_parcel",
+    "stylers": [{
+      "visibility": "off"
+    }]
+  },
+  {
+    "featureType": "administrative.land_parcel",
+    "elementType": "labels.text.fill",
+    "stylers": [{
+      "color": "#bdbdbd"
+    }]
+  },
+  {
+    "featureType": "administrative.neighborhood",
+    "stylers": [{
+      "visibility": "off"
+    }]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "geometry",
+    "stylers": [{
+      "color": "#eeeeee"
+    }]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "labels.text",
+    "stylers": [{
+      "visibility": "off"
+    }]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "labels.text.fill",
+    "stylers": [{
+      "color": "#757575"
+    }]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "geometry",
+    "stylers": [{
+      "color": "#e5e5e5"
+    }]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "labels.text.fill",
+    "stylers": [{
+      "color": "#9e9e9e"
+    }]
+  },
+  {
+    "featureType": "road",
+    "stylers": [{
+      "visibility": "off"
+    }]
+  },
+  {
+    "featureType": "road",
+    "elementType": "geometry",
+    "stylers": [{
+      "color": "#ffffff"
+    }]
+  },
+  {
+    "featureType": "road",
+    "elementType": "labels",
+    "stylers": [{
+      "visibility": "off"
+    }]
+  },
+  {
+    "featureType": "road.arterial",
+    "elementType": "labels.text.fill",
+    "stylers": [{
+      "color": "#757575"
+    }]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry",
+    "stylers": [{
+      "color": "#dadada"
+    }]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "labels.text.fill",
+    "stylers": [{
+      "color": "#616161"
+    }]
+  },
+  {
+    "featureType": "road.local",
+    "elementType": "labels.text.fill",
+    "stylers": [{
+      "color": "#9e9e9e"
+    }]
+  },
+  {
+    "featureType": "transit.line",
+    "elementType": "geometry",
+    "stylers": [{
+      "color": "#e5e5e5"
+    }]
+  },
+  {
+    "featureType": "transit.station",
+    "elementType": "geometry",
+    "stylers": [{
+      "color": "#eeeeee"
+    }]
+  },
+  {
+    "featureType": "water",
+    "elementType": "geometry",
+    "stylers": [{
+      "color": "#c9c9c9"
+    }]
+  },
+  {
+    "featureType": "water",
+    "elementType": "labels.text",
+    "stylers": [{
+      "visibility": "off"
+    }]
+  },
+  {
+    "featureType": "water",
+    "elementType": "labels.text.fill",
+    "stylers": [{
+      "color": "#9e9e9e"
+    }]
+  }
+];
+
+
+var pointStyles = {
+  'view': {
+    'color': '#000000',
+    'fillColor': '#000000',
+    'fillOpacity': 0.5,
+    'radius': 35000,
+    'pane': 'overlayPane'
+  },
+  'commitment': {
+    'color': 'red',
+    'fillColor': '#f03',
+    'fillOpacity': 0.5,
+    'radius': 75000,
+    'pane': 'markerPane'
+  }
+};
+
+var pageviews = 0;
+var commitments = 0;
+
+var lastUpdated = null;
 
 // Displays the points data provided.
 function displayPoints(data) {
 
   return new Promise(function(resolve, reject) {
+
     for (var i = 0; i < data.length; i++) {
-      var lookup = table.getFilteredRows([{column: 0, value: data[i].Coordinates[0]}, {column: 1, value: data[i].Coordinates[1]}]);
-      if (lookup.length > 0) {
-        table.setCell(lookup[0], data[i].Action === 'commitment' ? 2 : 3, (table.getValue(lookup[0], data[i].Action === 'commitment' ? 2 : 3)) + 1);
-      } else {
-      table.addRow([data[i].Coordinates[0], data[i].Coordinates[1], data[i].Action === 'commitment' ? 1 : 0, data[i].Action === 'view' ? 1 : 0]);
-    }
-      var options = {
-        sizeAxis: {
-          minValue: 0,
-          maxValue: 100
-        },
-        region: 'world',
-        displayMode: 'markers',
-        datalessRegionColor: 'rgb(204, 204, 204)',
-        colorAxis: {
-          colors: ['rgb(33, 150, 243)', 'rgb(63, 81, 181)', 'rgb(57, 73, 171)']
-        }
-      };
 
-      chart.draw(table, options);
-    }
+      // Create LatLng object
+      var LatLng = new google.maps.LatLng({
+        lat: data[i].Coordinates[0],
+        lng: data[i].Coordinates[1]
+      });
 
+      var circle = new google.maps.Circle({
+        fillColor: pointStyles[data[i].Action].fillColor,
+        fillOpacity: pointStyles[data[i].Action].fillOpacity,
+        map: map,
+        center: LatLng,
+        radius: pointStyles[data[i].Action].radius,
+        strokeWeight: 0
+      });
+
+      if (data[i].Action === 'view') {
+        pageviews++;
+      }
+      if (data[i].Action === 'commitment') {
+        commitments++;
+      }
+    }
+    document.getElementById("pageviews").innerText = pageviews;
+    document.getElementById("commitments").innerText = commitments;
     resolve('All points processed');
   });
+
 }
 
+
+// Updates map with latest real time data
 // https://developers.google.com/web/fundamentals/getting-started/primers/promises
 function get(url) {
 
@@ -51,15 +247,7 @@ function get(url) {
   return new Promise(function(resolve, reject) {
     // Do the usual XHR stuff
     var req = new XMLHttpRequest();
-
-    var lastUpdatedString = '';
-    if (lastUpdated != null) {
-      lastUpdatedString = '?lastupdated=' + lastUpdated;
-    }
-
-    lastUpdated = new Date() / 1000;
-
-    req.open('GET', url + lastUpdatedString);
+    req.open('GET', url);
     req.setRequestHeader('Content-Type', 'application/json');
     req.onload = function() {
       // This is called even on 404 etc
@@ -87,13 +275,20 @@ function get(url) {
 function callPromise() {
   return new Promise(function(resolve, reject) {
 
-    get('/rest/live/read').then(JSON.parse).then(displayPoints).then(function() {
-      console.log("callPromise Success!");
-      resolve('Success!');
-    }, function(error) {
-      console.error("callPromise Failed!", error);
-      reject(Error(error));
-    });
+  var lastUpdatedString = '';
+  if (lastUpdated != null) {
+    lastUpdatedString = '?lastupdated=' + lastUpdated;
+  }
+
+  lastUpdated = new Date() / 1000;
+
+  get('/rest/live/read' + lastUpdatedString).then(JSON.parse).then(displayPoints).then(function() {
+    console.log("callPromise Success!");
+    resolve('Success!');
+  }, function(error) {
+    console.error("callPromise Failed!", error);
+    reject(Error(error));
+  });
   });
 }
 
@@ -109,3 +304,5 @@ function refreshData() {
     setTimeout(refreshData, x * 1000);
   });
 }
+
+refreshData(); // execute function
