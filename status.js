cddc74986d28d774a40444bfc81f6d8183ad9f18
Kenan Scott <kenan@kenanscott.com>
2017-10-10 12:07:36 -0600

    Remove img alt text

--- status.js
+++ status.js
@@ -1,7 +1,7 @@
 // How long to wait after a statusGood call to display statusBad, in milliseconds.
 var waitTime = 5000;
 var element = document.getElementById('status');
-element.insertAdjacentHTML('beforeend', '<img id="status-icon" src="" alt="Status icon">');
+element.insertAdjacentHTML('beforeend', '<img id="status-icon" src="">');
 var icon = document.getElementById('status-icon');
 
 function statusGood(message) {
