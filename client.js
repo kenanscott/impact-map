a46b36181fd3df31926213ab9e5587ff23101586
Kenan Scott <kenan@kenanscott.com>
2017-12-19 14:21:36 -0700

    Fix indents

--- client.js
+++ client.js
@@ -2,213 +2,213 @@ var map = null;
 var legend = document.getElementById('legend');
 
 function initMap() {
-  // Create a map object and specify the DOM element for display.
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {
-      lat: 38.922239,
-      lng: -95.794675
-    },
-    disableDefaultUI: true,
-    styles: styles,
-    zoom: 5
-  });
-  map.controls[google.maps.ControlPosition.RIGHT_TOP].push(legend);
+	// Create a map object and specify the DOM element for display.
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: {
+			lat: 38.922239,
+			lng: -95.794675
+		},
+		disableDefaultUI: true,
+		styles: styles,
+		zoom: 5
+	});
+	map.controls[google.maps.ControlPosition.RIGHT_TOP].push(legend);
 
 }
 
 var styles = [{
-    "elementType": "geometry",
-    "stylers": [{
-      "color": "#f5f5f5"
-    }]
-  },
-  {
-    "elementType": "labels.icon",
-    "stylers": [{
-      "visibility": "off"
-    }]
-  },
-  {
-    "elementType": "labels.text.fill",
-    "stylers": [{
-      "color": "#616161"
-    }]
-  },
-  {
-    "elementType": "labels.text.stroke",
-    "stylers": [{
-      "color": "#f5f5f5"
-    }]
-  },
-  {
-    "featureType": "administrative.country",
-    "elementType": "geometry.stroke",
-    "stylers": [{
-      "color": "#666666"
-    }]
-  },
-  {
-    "featureType": "administrative.land_parcel",
-    "stylers": [{
-      "visibility": "off"
-    }]
-  },
-  {
-    "featureType": "administrative.land_parcel",
-    "elementType": "labels.text.fill",
-    "stylers": [{
-      "color": "#bdbdbd"
-    }]
-  },
-  {
-    "featureType": "administrative.neighborhood",
-    "stylers": [{
-      "visibility": "off"
-    }]
-  },
-  {
-    "featureType": "administrative.province",
-    "elementType": "geometry.stroke",
-    "stylers": [{
-      "color": "#808080"
-    }]
-  },
-  {
-    "featureType": "poi",
-    "elementType": "geometry",
-    "stylers": [{
-      "color": "#eeeeee"
-    }]
-  },
-  {
-    "featureType": "poi",
-    "elementType": "labels.text",
-    "stylers": [{
-      "visibility": "off"
-    }]
-  },
-  {
-    "featureType": "poi",
-    "elementType": "labels.text.fill",
-    "stylers": [{
-      "color": "#757575"
-    }]
-  },
-  {
-    "featureType": "poi.park",
-    "elementType": "geometry",
-    "stylers": [{
-      "color": "#e5e5e5"
-    }]
-  },
-  {
-    "featureType": "poi.park",
-    "elementType": "labels.text.fill",
-    "stylers": [{
-      "color": "#9e9e9e"
-    }]
-  },
-  {
-    "featureType": "road",
-    "stylers": [{
-      "visibility": "off"
-    }]
-  },
-  {
-    "featureType": "road",
-    "elementType": "geometry",
-    "stylers": [{
-      "color": "#ffffff"
-    }]
-  },
-  {
-    "featureType": "road",
-    "elementType": "labels",
-    "stylers": [{
-      "visibility": "off"
-    }]
-  },
-  {
-    "featureType": "road.arterial",
-    "elementType": "labels.text.fill",
-    "stylers": [{
-      "color": "#757575"
-    }]
-  },
-  {
-    "featureType": "road.highway",
-    "elementType": "geometry",
-    "stylers": [{
-      "color": "#dadada"
-    }]
-  },
-  {
-    "featureType": "road.highway",
-    "elementType": "labels.text.fill",
-    "stylers": [{
-      "color": "#616161"
-    }]
-  },
-  {
-    "featureType": "road.local",
-    "elementType": "labels.text.fill",
-    "stylers": [{
-      "color": "#9e9e9e"
-    }]
-  },
-  {
-    "featureType": "transit.line",
-    "elementType": "geometry",
-    "stylers": [{
-      "color": "#e5e5e5"
-    }]
-  },
-  {
-    "featureType": "transit.station",
-    "elementType": "geometry",
-    "stylers": [{
-      "color": "#eeeeee"
-    }]
-  },
-  {
-    "featureType": "water",
-    "elementType": "geometry",
-    "stylers": [{
-      "color": "#c9c9c9"
-    }]
-  },
-  {
-    "featureType": "water",
-    "elementType": "labels.text",
-    "stylers": [{
-      "visibility": "off"
-    }]
-  },
-  {
-    "featureType": "water",
-    "elementType": "labels.text.fill",
-    "stylers": [{
-      "color": "#9e9e9e"
-    }]
-  }
+	'elementType': 'geometry',
+	'stylers': [{
+		'color': '#f5f5f5'
+	}]
+},
+{
+	'elementType': 'labels.icon',
+	'stylers': [{
+		'visibility': 'off'
+	}]
+},
+{
+	'elementType': 'labels.text.fill',
+	'stylers': [{
+		'color': '#616161'
+	}]
+},
+{
+	'elementType': 'labels.text.stroke',
+	'stylers': [{
+		'color': '#f5f5f5'
+	}]
+},
+{
+	'featureType': 'administrative.country',
+	'elementType': 'geometry.stroke',
+	'stylers': [{
+		'color': '#666666'
+	}]
+},
+{
+	'featureType': 'administrative.land_parcel',
+	'stylers': [{
+		'visibility': 'off'
+	}]
+},
+{
+	'featureType': 'administrative.land_parcel',
+	'elementType': 'labels.text.fill',
+	'stylers': [{
+		'color': '#bdbdbd'
+	}]
+},
+{
+	'featureType': 'administrative.neighborhood',
+	'stylers': [{
+		'visibility': 'off'
+	}]
+},
+{
+	'featureType': 'administrative.province',
+	'elementType': 'geometry.stroke',
+	'stylers': [{
+		'color': '#808080'
+	}]
+},
+{
+	'featureType': 'poi',
+	'elementType': 'geometry',
+	'stylers': [{
+		'color': '#eeeeee'
+	}]
+},
+{
+	'featureType': 'poi',
+	'elementType': 'labels.text',
+	'stylers': [{
+		'visibility': 'off'
+	}]
+},
+{
+	'featureType': 'poi',
+	'elementType': 'labels.text.fill',
+	'stylers': [{
+		'color': '#757575'
+	}]
+},
+{
+	'featureType': 'poi.park',
+	'elementType': 'geometry',
+	'stylers': [{
+		'color': '#e5e5e5'
+	}]
+},
+{
+	'featureType': 'poi.park',
+	'elementType': 'labels.text.fill',
+	'stylers': [{
+		'color': '#9e9e9e'
+	}]
+},
+{
+	'featureType': 'road',
+	'stylers': [{
+		'visibility': 'off'
+	}]
+},
+{
+	'featureType': 'road',
+	'elementType': 'geometry',
+	'stylers': [{
+		'color': '#ffffff'
+	}]
+},
+{
+	'featureType': 'road',
+	'elementType': 'labels',
+	'stylers': [{
+		'visibility': 'off'
+	}]
+},
+{
+	'featureType': 'road.arterial',
+	'elementType': 'labels.text.fill',
+	'stylers': [{
+		'color': '#757575'
+	}]
+},
+{
+	'featureType': 'road.highway',
+	'elementType': 'geometry',
+	'stylers': [{
+		'color': '#dadada'
+	}]
+},
+{
+	'featureType': 'road.highway',
+	'elementType': 'labels.text.fill',
+	'stylers': [{
+		'color': '#616161'
+	}]
+},
+{
+	'featureType': 'road.local',
+	'elementType': 'labels.text.fill',
+	'stylers': [{
+		'color': '#9e9e9e'
+	}]
+},
+{
+	'featureType': 'transit.line',
+	'elementType': 'geometry',
+	'stylers': [{
+		'color': '#e5e5e5'
+	}]
+},
+{
+	'featureType': 'transit.station',
+	'elementType': 'geometry',
+	'stylers': [{
+		'color': '#eeeeee'
+	}]
+},
+{
+	'featureType': 'water',
+	'elementType': 'geometry',
+	'stylers': [{
+		'color': '#c9c9c9'
+	}]
+},
+{
+	'featureType': 'water',
+	'elementType': 'labels.text',
+	'stylers': [{
+		'visibility': 'off'
+	}]
+},
+{
+	'featureType': 'water',
+	'elementType': 'labels.text.fill',
+	'stylers': [{
+		'color': '#9e9e9e'
+	}]
+}
 ];
 
 
 var pointStyles = {
-  'view': {
-    'color': 'rgb(33, 150, 243)',
-    'fillColor': 'rgb(33, 150, 243)',
-    'fillOpacity': 0.2,
-    'radius': 35000,
-    'pane': 'overlayPane'
-  },
-  'commitment': {
-    'color': 'rgb(29, 41, 114)',
-    'fillColor': 'rgb(29, 41, 114)',
-    'fillOpacity': 0.6,
-    'radius': 55000,
-    'pane': 'markerPane'
-  }
+	'view': {
+		'color': 'rgb(33, 150, 243)',
+		'fillColor': 'rgb(33, 150, 243)',
+		'fillOpacity': 0.2,
+		'radius': 35000,
+		'pane': 'overlayPane'
+	},
+	'commitment': {
+		'color': 'rgb(29, 41, 114)',
+		'fillColor': 'rgb(29, 41, 114)',
+		'fillOpacity': 0.6,
+		'radius': 55000,
+		'pane': 'markerPane'
+	}
 };
 
 var pageviews = 0;
@@ -222,42 +222,42 @@ var lastEvaluatedKey;
 // Displays the points data provided.
 function displayPoints(data) {
 
-  return new Promise(function(resolve, reject) {
+	return new Promise(function(resolve, reject) {
 
-    if (data.hasOwnProperty('LastEvaluatedKey')) {
-      lastEvaluatedKey = data.LastEvaluatedKey.Id;
-    } else {
-      lastEvaluatedKey = 'finished';
-     }
+		if (data.hasOwnProperty('LastEvaluatedKey')) {
+			lastEvaluatedKey = data.LastEvaluatedKey.Id;
+		} else {
+			lastEvaluatedKey = 'finished';
+		}
 
-    for (var i = 0; i < data.Items.length; i++) {
+		for (var i = 0; i < data.Items.length; i++) {
 
-      // Create LatLng object
-      var LatLng = new google.maps.LatLng({
-        lat: data.Items[i].Coordinates[0],
-        lng: data.Items[i].Coordinates[1]
-      });
+			// Create LatLng object
+			var LatLng = new google.maps.LatLng({
+				lat: data.Items[i].Coordinates[0],
+				lng: data.Items[i].Coordinates[1]
+			});
 
-      var circle = new google.maps.Circle({
-        fillColor: pointStyles[data.Items[i].Action].fillColor,
-        fillOpacity: pointStyles[data.Items[i].Action].fillOpacity,
-        map: map,
-        center: LatLng,
-        radius: pointStyles[data.Items[i].Action].radius,
-        strokeWeight: 0
-      });
+			var circle = new google.maps.Circle({
+				fillColor: pointStyles[data.Items[i].Action].fillColor,
+				fillOpacity: pointStyles[data.Items[i].Action].fillOpacity,
+				map: map,
+				center: LatLng,
+				radius: pointStyles[data.Items[i].Action].radius,
+				strokeWeight: 0
+			});
 
-      if (data.Items[i].Action === 'view') {
-        pageviews++;
-      }
-      if (data.Items[i].Action === 'commitment') {
-        commitments++;
-      }
-    }
-    document.getElementById("pageviews").innerText = pageviews;
-    document.getElementById("commitments").innerText = commitments;
-    resolve('All points processed');
-  });
+			if (data.Items[i].Action === 'view') {
+				pageviews++;
+			}
+			if (data.Items[i].Action === 'commitment') {
+				commitments++;
+			}
+		}
+		document.getElementById('pageviews').innerText = pageviews;
+		document.getElementById('commitments').innerText = commitments;
+		resolve('All points processed');
+	});
 
 }
 
@@ -266,88 +266,88 @@ function displayPoints(data) {
 // https://developers.google.com/web/fundamentals/getting-started/primers/promises
 function get(url) {
 
-  // Return a new promise.
-  return new Promise(function(resolve, reject) {
-    // Do the usual XHR stuff
-    var req = new XMLHttpRequest();
-    req.open('GET', url);
-    req.setRequestHeader('Content-Type', 'application/json');
-    req.onload = function() {
-      // This is called even on 404 etc
-      // so check the status
-      if (req.status == 200) {
-        // Resolve the promise with the response text
-        resolve(req.response);
-      } else {
-        // Otherwise reject with the status text
-        // which will hopefully be a meaningful error
-        statusBad('Disconnected, server error');
-        reject(Error(req.statusText));
-      }
-    };
+	// Return a new promise.
+	return new Promise(function(resolve, reject) {
+		// Do the usual XHR stuff
+		var req = new XMLHttpRequest();
+		req.open('GET', url);
+		req.setRequestHeader('Content-Type', 'application/json');
+		req.onload = function() {
+			// This is called even on 404 etc
+			// so check the status
+			if (req.status == 200) {
+				// Resolve the promise with the response text
+				resolve(req.response);
+			} else {
+				// Otherwise reject with the status text
+				// which will hopefully be a meaningful error
+				statusBad('Disconnected, server error');
+				reject(Error(req.statusText));
+			}
+		};
 
-    // Handle network errors
-    req.onerror = function() {
-      statusBad('Disconnected, network error');
-      reject(Error("Network Error"));
-    };
+		// Handle network errors
+		req.onerror = function() {
+			statusBad('Disconnected, network error');
+			reject(Error('Network Error'));
+		};
 
-    // Make the request
-    req.send();
-  });
+		// Make the request
+		req.send();
+	});
 }
 
 function delay(t) {
-  return new Promise(function(resolve) {
-    setTimeout(resolve, t);
-  });
+	return new Promise(function(resolve) {
+		setTimeout(resolve, t);
+	});
 }
 
 // https://medium.com/adobe-io/how-to-combine-rest-api-calls-with-javascript-promises-in-node-js-or-openwhisk-d96cbc10f299
 // https://gist.github.com/trieloff/168312d4dd4d149afdd55cde3d3724cabea
 var promiseChain = {
-  runChain: function() {
-    var timeString = '';
-    if (from === null) {
-      var d = new Date();
-      d.setHours(0, 0, 0, 0);
-      d = d.getTime() / 1000;
+	runChain: function() {
+		var timeString = '';
+		if (from === null) {
+			var d = new Date();
+			d.setHours(0, 0, 0, 0);
+			d = d.getTime() / 1000;
 
-      if (to === null) {
-        to = new Date() / 1000;
-      }
-      timeString = 'from=' + d + '&to=' + to + '&'; // If from is not set, set to midnight local time in seconds (accurate to the millisecond)
-    } else {
-      if (to === null) {
-        to = new Date() / 1000;
-      }
-      timeString = 'from=' + from + '&to=' + to + '&';
-    }
+			if (to === null) {
+				to = new Date() / 1000;
+			}
+			timeString = 'from=' + d + '&to=' + to + '&'; // If from is not set, set to midnight local time in seconds (accurate to the millisecond)
+		} else {
+			if (to === null) {
+				to = new Date() / 1000;
+			}
+			timeString = 'from=' + from + '&to=' + to + '&';
+		}
 
-    var exclusiveStartKeyString = '';
-    if (lastEvaluatedKey != undefined && lastEvaluatedKey != 'finished') {
-      exclusiveStartKeyString = 'exclusivestartkey=' + lastEvaluatedKey;
-    }
+		var exclusiveStartKeyString = '';
+		if (lastEvaluatedKey != undefined && lastEvaluatedKey != 'finished') {
+			exclusiveStartKeyString = 'exclusivestartkey=' + lastEvaluatedKey;
+		}
 
-    get('/rest/live/read?' + timeString + exclusiveStartKeyString).then(JSON.parse).then(displayPoints).then(function() {
-      if (lastEvaluatedKey !== 'finished') {
-        promiseChain.runChain();
-      } else {
-        // Done paginating through data
-        from = to + 0.001;
-        to = null;
-        statusGood('Map connected live');
-        return delay(3000).then(function() {
-          promiseChain.runChain();
-        });
-      }
-    }, function(error) {
-      console.error("callPromise Failed!", error);
-      return delay(3000).then(function() {
-        promiseChain.runChain();
-      });
-    }); // Anonymous function
-  } // runChain
+		get('/rest/live/read?' + timeString + exclusiveStartKeyString).then(JSON.parse).then(displayPoints).then(function() {
+			if (lastEvaluatedKey !== 'finished') {
+				promiseChain.runChain();
+			} else {
+				// Done paginating through data
+				from = to + 0.001;
+				to = null;
+				statusGood('Map connected live');
+				return delay(3000).then(function() {
+					promiseChain.runChain();
+				});
+			}
+		}, function(error) {
+			console.error('callPromise Failed!', error);
+			return delay(3000).then(function() {
+				promiseChain.runChain();
+			});
+		}); // Anonymous function
+	} // runChain
 }; // promiseChain
 
 var midnight = new Date();
@@ -358,13 +358,13 @@ var now = new Date() / 1000;
 midnight = midnight - now;
 midnight = midnight + 5; // Add 5 seconds as a cushion
 setTimeout(
-  midnightTask,
-  midnight * 1000
+	midnightTask,
+	midnight * 1000
 );
 
 
 function midnightTask() {
-  window.location.reload(true);
+	window.location.reload(true);
 }
 
 promiseChain.runChain(); // execute function
--- read.js
+++ read.js
@@ -9,72 +9,72 @@ const moment = require('moment-timezone');
 
 exports.handler = (event, context, callback) => {
 
-  let from;
-  if (Number(event.from) > 1) {
-    console.log('Detecting from variable, ' + event.from);
-    from = Number(event.from);
-  } else {
-    from = 1;
-  }
+	let from;
+	if (Number(event.from) > 1) {
+		console.log('Detecting from variable, ' + event.from);
+		from = Number(event.from);
+	} else {
+		from = 1;
+	}
 
-  let to;
-  if (Number(event.to) > 1) {
-    console.log('Detecting to variable, ' + event.to);
-    to = Number(event.to);
-  } else {
-    to = new Date() / 1000; // If to isn't present, default to current time
-  }
+	let to;
+	if (Number(event.to) > 1) {
+		console.log('Detecting to variable, ' + event.to);
+		to = Number(event.to);
+	} else {
+		to = new Date() / 1000; // If to isn't present, default to current time
+	}
 
-  // Set scan limit to 20 if not present or greater than 20
-  let limit = process.env.LIMIT;
-  if (Number(event.limit) < process.env.LIMIT) {
-    console.log('Detecting limit variable ' + event.limit);
-    limit = event.limit;
-  }
+	// Set scan limit to 20 if not present or greater than 20
+	let limit = process.env.LIMIT;
+	if (Number(event.limit) < process.env.LIMIT) {
+		console.log('Detecting limit variable ' + event.limit);
+		limit = event.limit;
+	}
 
-  console.log('Scanning from greater than ' + from);
-  // Set scanning parameters
-  let params = {
-    TableName: 'impact-map',
-    // Only get Coordinates and Action data from the table
-    ProjectionExpression: 'Coordinates, #b',
-    // Only return items that fall within start and stop parameters
-    FilterExpression: 'Added BETWEEN :start AND :stop',
-    ExpressionAttributeNames: {
-      // Set an 'alias' for the word, 'Action', because 'Action' is a reserved word in DynamoDB
-      '#b': 'Action'
-    },
-    ExpressionAttributeValues: {
-      // Set an 'alias' for start and stop variables
-      ':start': from,
-      ':stop': to
-    },
-    Limit: limit
-  };
+	console.log('Scanning from greater than ' + from);
+	// Set scanning parameters
+	let params = {
+		TableName: 'impact-map',
+		// Only get Coordinates and Action data from the table
+		ProjectionExpression: 'Coordinates, #b',
+		// Only return items that fall within start and stop parameters
+		FilterExpression: 'Added BETWEEN :start AND :stop',
+		ExpressionAttributeNames: {
+			// Set an 'alias' for the word, 'Action', because 'Action' is a reserved word in DynamoDB
+			'#b': 'Action'
+		},
+		ExpressionAttributeValues: {
+			// Set an 'alias' for start and stop variables
+			':start': from,
+			':stop': to
+		},
+		Limit: limit
+	};
 
-  if (event.hasOwnProperty('exclusivestartkey') && event.exclusivestartkey !== '') {
-    console.log('exclusivestartkey detected ' + event.exclusivestartkey);
-    params.ExclusiveStartKey = {};
-    params.ExclusiveStartKey.Id = event.exclusivestartkey;
-  }
+	if (event.hasOwnProperty('exclusivestartkey') && event.exclusivestartkey !== '') {
+		console.log('exclusivestartkey detected ' + event.exclusivestartkey);
+		params.ExclusiveStartKey = {};
+		params.ExclusiveStartKey.Id = event.exclusivestartkey;
+	}
 
-  console.log('Scanning table.');
-  dynamodb.scan(params, onScan);
+	console.log('Scanning table.');
+	dynamodb.scan(params, onScan);
 
-  function onScan(err, data) {
-    // Log error if scanning returns err
-    if (err) {
-      console.error('Unable to scan the table. Error JSON:', JSON.stringify(err, null, 2));
-      callback(err);
-    } else {
-      // Continue scanning if we have more items, because scan can retrieve a maximum of 1MB of data
-      if (data.hasOwnProperty('LastEvaluatedKey')) {
-        console.log('LastEvaluatedKey detected');
-      }
+	function onScan(err, data) {
+		// Log error if scanning returns err
+		if (err) {
+			console.error('Unable to scan the table. Error JSON:', JSON.stringify(err, null, 2));
+			callback(err);
+		} else {
+			// Continue scanning if we have more items, because scan can retrieve a maximum of 1MB of data
+			if (data.hasOwnProperty('LastEvaluatedKey')) {
+				console.log('LastEvaluatedKey detected');
+			}
 
-      // Returns the item data back to the client
-      console.log('Returning ' + data.Items.Count + ' items to the client');
-      callback(null, data);
-    }
-  }
+			// Returns the item data back to the client
+			console.log('Returning ' + data.Items.Count + ' items to the client');
+			callback(null, data);
+		}
+	}
 };
